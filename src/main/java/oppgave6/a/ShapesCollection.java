8553d56ce4f3a7381a267c893ebe46ee736ff766
ervikleo <leo-andreas.ervik@gmail.com>
2016-11-14 11:51:49 +0100

    added fasit branch

--- src/main/java/oppgave6/a/ShapesCollection.java
+++ src/main/java/oppgave6/a/ShapesCollection.java
@@ -4,6 +4,7 @@ package oppgave6.a;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
+import java.util.stream.Collectors;
 
 /**
  * 14.11.2016
@@ -23,26 +24,30 @@ public class ShapesCollection {
 
     public ShapesCollection() {
         shapes = new ArrayList<>();
-        populateRandom(10000);
+        populateRandom(1000000);
     }
 
     public List<Shape> sortShapesByHeightUsingSequentialStream() {
-        List<Shape> temp = new ArrayList<>();
-        Long time = System.currentTimeMillis();
-        /*
-            WRITE SEQUENTIAL STREAM SORT HERE
-         */
-        System.out.println("Elapsed time, : " + (System.currentTimeMillis() - time));
+        List<Shape> temp;
+        long time = System.currentTimeMillis();
+
+        temp = shapes.stream()
+                .sorted((s1, s2) -> Double.compare(s2.getHeight(), s1.getHeight()))
+                .collect(Collectors.toList());
+
+        System.out.println("Elapsed time, sequential: " + (System.currentTimeMillis() - time) + " ms");
         return temp;
     }
 
     public List<Shape> sortShapesByHeightUsingParrallelStream() {
-        List<Shape> temp = new ArrayList<>();
-        Long time = System.currentTimeMillis();
-        /*
-            WRITE PARALLEL STREAM SORT HERE
-         */
-        System.out.println("Elapsed time: " + (System.currentTimeMillis() - time));
+        List<Shape> temp;
+        long time = System.currentTimeMillis();
+
+        temp = shapes.parallelStream()
+                .sorted((s1, s2) -> Double.compare(s2.getHeight(), s1.getHeight()))
+                .collect(Collectors.toList());
+
+        System.out.println("Elapsed time, parallel: " + (System.currentTimeMillis() - time) + " ms");
         return temp;
     }
 
